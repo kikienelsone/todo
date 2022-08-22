d032be1cd4941eac4d2626e89deefb6dd64f42d4
kikienelsone <81746445+kikienelsone@users.noreply.github.com>
2022-08-22 20:47:45 +0300

    commit

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -1,39 +1,39 @@
-import "./NewTaskForm.css"
-import React from "react";
+import './NewTaskForm.css'
+import React from 'react'
 
-
-export default class NewTaskForm extends React.Component{
-    constructor(props) {
-        super(props)
+export default class NewTaskForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: '',
     }
+  }
 
-    state = {
-        input : ""
-    }
+  addNewTask(event) {
+    this.setState({
+      input: event.target.value,
+    })
+  }
 
-    addNewTask = (event) =>{
-        this.setState({
-            input : event.target.value
-        })
-    }
+  onSubmit(event) {
+    event.preventDefault()
+    this.props.addItem(this.state.input)
+    this.setState({
+      input: '',
+    })
+  }
 
-    onSubmit = (event) =>{
-        event.preventDefault()
-        this.props.addItem(this.state.input)
-        this.setState({
-            input : ""
-        })
-    }
-
-    render() {
-        return(
-            <form onSubmit={this.onSubmit}>
-                <input className="new-todo" placeholder="What needs to be done?"
-                       autoFocus onChange={this.addNewTask} value={this.state.input}/>
-
-            </form>
-
-        )
-    }
-
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          className="new-todo"
+          placeholder="What needs to be done?"
+          autoFocus
+          onChange={this.addNewTask}
+          value={this.state.input}
+        />
+      </form>
+    )
+  }
 }
