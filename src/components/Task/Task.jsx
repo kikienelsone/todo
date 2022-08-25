c104e107d01a91a632f9c3994eee3e9738fd51db
kikienelsone <81746445+kikienelsone@users.noreply.github.com>
2022-08-25 12:49:47 +0300

    commit

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -19,13 +19,8 @@ export default class Task extends React.Component {
   render() {
     const { done, onDone, date, onDelete, id, task } = this.props
     let addedClassNames = ''
-    let editClassName = ''
     if (done) {
       addedClassNames = ' completed'
-    }
-
-    if (!editClassName) {
-      editClassName = 'edit'
     }
 
     return (
@@ -41,20 +36,20 @@ export default class Task extends React.Component {
             <button className="icon icon-destroy" onClick={() => onDelete(id)} />
           </div>
         </li>
-        <div className={editClassName}>
-          <li className="editing">
-            <div className="view">
-              <input className="toggle" type="checkbox" />
-              <label>
-                <span className="description">{task}</span>
-                <span className="created">created {date(task)}</span>
-              </label>
-              <button onClick={() => editClassName === 'edit'} className="icon icon-edit" />
-              <button className="icon icon-destroy" />
-            </div>
-            <input type="text" className="edit" value="Editing task" />
-          </li>
-        </div>
+        {/*<div>*/}
+        {/*  <li className="editing">*/}
+        {/*    <div className="view">*/}
+        {/*      <input className="toggle" type="checkbox" />*/}
+        {/*      <label>*/}
+        {/*        <span className="description">{task}</span>*/}
+        {/*        <span className="created">created {date(task)}</span>*/}
+        {/*      </label>*/}
+        {/*      <button className="icon icon-edit" />*/}
+        {/*      <button className="icon icon-destroy" />*/}
+        {/*    </div>*/}
+        {/*    <input type="text" className="edit" value="Editing task" />*/}
+        {/*  </li>*/}
+        {/*</div>*/}
       </div>
     )
   }
